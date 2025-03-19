dbfd1876786eb259d6db79ae3a84f942cb796b75
farzad taherzadeh <farzade.taherzade@gmail.com>
2025-03-20 03:27:21 +0330

    add response return type to auth module

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { ApiResponse } from 'src/shared/responses/api.response';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
@@ -11,21 +14,46 @@ export class AuthenticationController {
   @Post('sign-in')
   async signIn(@Body() signinDto: SigninDto): Promise<
     ApiResponse<{
-      accessToken: string;
-      refreshToken: string;
+      access_token: string;
+      refresh_token: string;
     }>
   > {
-    const {} = await this.authenticationService.signin(signinDto);
-    return;
+    const { access_token, refresh_token } =
+      await this.authenticationService.signin(signinDto);
+    return new ApiResponse(
+      { access_token, refresh_token },
+      'Signin successful',
+      HttpStatus.OK,
+    );
   }
 
   @Post('sign-up')
-  async signup(@Body() signupDto: SignupDto) {
-    return await this.authenticationService.signup(signupDto);
+  async signup(@Body() signupDto: SignupDto): Promise<
+    ApiResponse<{
+      message: string;
+    }>
+  > {
+    const { message } = await this.authenticationService.signup(signupDto);
+    return new ApiResponse(
+      { message },
+      'Signup successful',
+      HttpStatus.CREATED,
+    );
   }
 
   @Post('refresh-token')
-  refresh(@Body() dto: RefreshTokenDto) {
-    return this.authenticationService.refresh(dto.token);
+  async refresh(@Body() dto: RefreshTokenDto): Promise<
+    ApiResponse<{
+      access_token: string;
+      refresh_token: string;
+    }>
+  > {
+    const { access_token, refresh_token } =
+      await this.authenticationService.refresh(dto.token);
+    return new ApiResponse(
+      { access_token, refresh_token },
+      'Token refreshed',
+      HttpStatus.OK,
+    );
   }
 }
